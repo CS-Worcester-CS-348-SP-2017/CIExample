611f70027c5cff8e4efa432dad641994a4c492c7
osworup007 <osworup007@users.noreply.github.com>
2017-02-28 13:50:52 -0500

    Update EndsWith3.java

--- src/main/java/CS_348_SP_2017/CIExample/EndsWith3.java
+++ src/main/java/CS_348_SP_2017/CIExample/EndsWith3.java
@@ -5,9 +5,10 @@ public class EndsWith3 {
 	String names;
 
 	public EndsWith3()  {
-names= "";
-		names+= "Sworup Ojha\n";
+		names = "";
 		// add your name here in the form of a line that looks like:
+		// names+= "John Smith\n";
+		names+= "Sworup Ojha\n";
 		names+= "Connor Virzi\n";
 	}
 
