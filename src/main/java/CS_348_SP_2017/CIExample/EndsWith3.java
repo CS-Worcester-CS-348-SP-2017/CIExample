8edbc8cc1f785c0de429720a9bcce6aa118adc50
Sworup Ojha <Sworup Ojha>
2017-02-28 09:31:53 -0500

    Commit 1

--- src/main/java/CS_348_SP_2017/CIExample/EndsWith3.java
+++ src/main/java/CS_348_SP_2017/CIExample/EndsWith3.java
@@ -5,7 +5,8 @@ public class EndsWith3 {
 	String names;
 
 	public EndsWith3()  {
-		names = "";
+names= "";
+		names+= "Sworup Ojha\n";
 		// add your name here in the form of a line that looks like:
 		// names+= "John Smith\n";
 	}
