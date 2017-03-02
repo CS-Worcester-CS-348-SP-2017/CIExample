667d74450cf6cd2b2247fc45ac4e452c77a613cd
Angelito Destin <adestin@worcester.edu>
2017-03-02 09:10:34 -0500

    added-Angelito Destin

--- src/main/java/CS_348_SP_2017/CIExample/EndsWith0.java
+++ src/main/java/CS_348_SP_2017/CIExample/EndsWith0.java
@@ -1,13 +1,14 @@
 package CS_348_SP_2017.CIExample;
 
 public class EndsWith0 {
-	
+
 	String names;
 
 	public EndsWith0()  {
-		names = "";
+		names+= "";
 		// add your name here in the form of a line that looks like:
 		// names+= "John Smith\n"
+		names+= "Angelito Destin\n";
 	}
 
 	@Override
